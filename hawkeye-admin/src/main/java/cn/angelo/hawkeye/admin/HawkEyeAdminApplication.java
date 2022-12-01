772286fe923b1cf6b84bc707cb24f0d3326fbe5d
君影草 <junyingcao@myweimai.com>
2022-12-01 16:07:43 +0800

    fix(websokcet连接不上问题修复):

--- hawkeye-admin/src/main/java/cn/angelo/hawkeye/admin/HawkEyeAdminApplication.java
+++ hawkeye-admin/src/main/java/cn/angelo/hawkeye/admin/HawkEyeAdminApplication.java
@@ -2,6 +2,7 @@ package cn.angelo.hawkeye.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 public class HawkEyeAdminApplication {
--- hawkeye-demo/src/main/java/cn/angelo/hawkeye/demo/HawkEyeDemoApplication.java
+++ hawkeye-demo/src/main/java/cn/angelo/hawkeye/demo/HawkEyeDemoApplication.java
@@ -3,6 +3,7 @@ package cn.angelo.hawkeye.demo;
 import cn.angelo.hawkeye.spring.boot.EnableHawkEye;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @EnableHawkEye
 @SpringBootApplication
